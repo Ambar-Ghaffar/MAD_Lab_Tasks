7036400da2db446b21e4277dcc5a15618e47dd18
Ambar-Ghaffar <ambaraarain@gmail.com>
2023-10-17 21:59:17 +0500

    Navigation_task

---
+++ lib/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'detail.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailsScreen()),
+            );
+          },
+          child: Text('Go to Details page'),
+        ),
+      ),
+    );
+  }
+}
